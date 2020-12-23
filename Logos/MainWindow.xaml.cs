ad1f9fc23ab382ff2ebcf1ec17f525f9b94ef49f
Joseph Chen <joseph@baoruh.com.tw>
2020-12-23 18:15:42 +0800

    [feat] Create menu panel

--- Logos/MainWindow.xaml.cs
+++ Logos/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SourceChord.FluentWPF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,8 +19,10 @@ namespace Logos
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        public static string ProductName => Application.ResourceAssembly.GetName().Name;
+
         public MainWindow()
         {
             InitializeComponent();
