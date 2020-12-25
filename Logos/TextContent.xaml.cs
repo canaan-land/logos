593f4f6afadbcf80c2c473d2bd098b7f91fe1d19
Joseph Chen <joseph@baoruh.com.tw>
2020-12-25 09:07:07 +0800

    [nit] Sylte change

--- Logos/TextContent.xaml.cs
+++ Logos/TextContent.xaml.cs
@@ -22,8 +22,8 @@ namespace Logos
     /// </summary>
     public partial class TextContent : UserControl
     {
-        public static ObservableCollection<string> FontCollection => new ObservableCollection<string>(
-            from font in new InstalledFontCollection().Families select font.Name);
+        public static ObservableCollection<string> FontCollection => new ObservableCollection<string>
+            (from font in new InstalledFontCollection().Families select font.Name);
 
         public TextContent()
         {
